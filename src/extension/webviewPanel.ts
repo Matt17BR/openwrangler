e782f987e8d0e31f7c37c6fc963bce0f4f874e02
Matt17BR <matteo.mazzarelli@gmail.com>
2026-08-16 16:17:39 +0200

    fix(webview): block remote workbench images

--- src/extension/webviewPanel.ts
+++ src/extension/webviewPanel.ts
@@ -1937,7 +1937,7 @@ export class OpenWranglerPanel {
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${webview.cspSource} https:; style-src ${webview.cspSource} 'unsafe-inline'; font-src ${webview.cspSource}; script-src ${webview.cspSource} 'nonce-${nonce}';">
+  <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${webview.cspSource}; style-src ${webview.cspSource} 'unsafe-inline'; font-src ${webview.cspSource}; script-src ${webview.cspSource} 'nonce-${nonce}';">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="${styleUri}">
   <title>Open Wrangler</title>
